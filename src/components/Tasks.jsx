e25e9ea74706578e8f1ecf6822908bcb484278e8
Matheus Sousa <devlpsousa@gmail.com>
2024-02-21 11:31:01 -0300

    feat: add scrollbar in tasks no completeds

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import TaskItem from "./TaskItem/TaskItem";
 import AddTask from "./AddTask/AddTask";
+import "./Tasks.scss";
 
 export default function Tasks() {
   const [tasks, setTasks] = useState([]);
@@ -21,32 +22,33 @@ export default function Tasks() {
 
   return (
     <>
-      <header className=" p-5 bg-gray-950">
+      <header className="p-5 bg-gray-950">
         <h1 className="text-3xl font-bold">Dev List</h1>
       </header>
-      <div className="flex flex-col p-2 max-h-[100vh]">
+      <div className="flex flex-col p-2">
         <div className="flex flex-wrap gap-52 p-5">
           <div className="flex flex-col">
             <h1 className="font-bold text-xl pb-3">Adicionar Tarefa</h1>
 
             <div className="flex mb-5">
-              <AddTask />
+              <AddTask fetchTasks={fetchTasks} />
             </div>
 
-            <div className="tasks-lists">
-              <h3 className="my-3 font-bold text-xl">Tarefas não Concluídas</h3>
-
-              {tasks
-                .filter((task) => !task.isCompleted)
-                .map((lastTask, index) => (
-                  <div key={index}>
-                    <TaskItem task={lastTask} />
-                  </div>
-                ))}
+            <div>
+              <h3 className="font-bold text-xl mb-3">Tarefas não Concluídas</h3>
+              <div className="last-tasks">
+                {tasks
+                  .filter((task) => !task.isCompleted)
+                  .map((lastTask, index) => (
+                    <div key={index}>
+                      <TaskItem task={lastTask} />
+                    </div>
+                  ))}
+              </div>
             </div>
           </div>
-          <div className="flex-1">
-            <h2 className="font-bold text-xl ">Tarefas Concluídas</h2>
+          <div>
+            <h2 className="font-bold text-xl">Tarefas Concluídas</h2>
             <div className="tasks-lists">
               {tasks
                 .filter((task) => task.isCompleted)
